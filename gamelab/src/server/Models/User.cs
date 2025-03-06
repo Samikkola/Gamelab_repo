136554eac5699717d1600802f5ce188a660e275d
Sami Mikkola <fsami009@edu.xamk.fi>
2025-03-06 10:58:09 +0200

    User, usercontroller, appdbcontext ja gitignore tiedostot luotu

---
+++ gamelab/src/server/Models/User.cs
@@ -0,0 +1,8 @@
+namespace gamelab.src.server.Models
+{
+    public class User
+    {
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+    }   
+}
--- gamelab/src/server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ gamelab/src/server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88418d12e93020b5ff24a2bb7a1a401d99262de3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a711d006de2c6820ac574c36c34725a55dcd56")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
